e523a1404381b9c04f2c5be86cb62c90dcef0d33
MiqueasC <miqueas@pow-gaming.com>
2026-05-06 20:16:57 -0300

    fix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,6 @@ from app.models.user import User
 from app.models.role import Role
 from app.models.permission import Permission
 from app.models.permission_role import PermissionRole
+from app.models.project import Project
+from app.models.project_document import ProjectDocument
+from app.models.project_advance import ProjectAdvance
--- app/models/project.py
+++ app/models/project.py
@@ -13,9 +13,9 @@ from app.models.category_project import CategoryProject
 if TYPE_CHECKING:
     from app.models.user import User
     from app.models.category import Category
-    from app.models.project_document import ProjectDocument
-    from app.models.project_advance import ProjectAdvance
 
+from app.models.project_document import ProjectDocument
+from app.models.project_advance import ProjectAdvance
 class Project(Base):
     __tablename__ = "PROJECT"
     
